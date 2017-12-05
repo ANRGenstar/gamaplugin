5cfc62fe6085889f8cd214bd0fe29222698b7078
rey-coyrehourcq <reyman@localhost.localdomain>
2017-12-05 04:38:33 +0100

    fix way to generate

--- genstar.plugin.bundle-all/src/main/java/gama/genstar/plugin/Foo.java
+++ genstar.plugin.bundle-all/src/main/java/gama/genstar/plugin/Foo.java
@@ -1,37 +1,8 @@
 package gama.genstar.plugin;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
-
-import msi.gama.common.util.FileUtils;
-import msi.gama.metamodel.shape.GamaShape;
-import msi.gama.metamodel.shape.IShape;
 import msi.gama.precompiler.GamlAnnotations.doc;
-import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.operator;
-import msi.gama.runtime.GAMA;
-import msi.gama.runtime.IScope;
-import msi.gama.runtime.exceptions.GamaRuntimeException;
-import msi.gama.util.GamaListFactory;
-import msi.gama.util.GamaMap;
-import msi.gama.util.GamaMapFactory;
-import msi.gama.util.IList;
-import msi.gaml.operators.Cast;
-import msi.gaml.operators.Spatial;
-import msi.gaml.types.IType;
-import msi.gaml.types.Types;
+
 
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class Foo {
