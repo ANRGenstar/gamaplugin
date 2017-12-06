c97b7695140322f772fb8387ec5c4fb96d91c918
rey-coyrehourcq <reyman@localhost.localdomain>
2017-12-06 15:26:47 +0100

    add credentials try 1

--- travisbuild.sh
+++ travisbuild.sh
@@ -2,4 +2,4 @@
 echo "genstarpassw=xMY2d63UAUixj3m/AXuCTg\=\=" >> ~/.gradle/gradle.6346B4DDA13C97C4C0BE190A1D1F519E.encrypted.properties
 cat ~/.gradle/gradle.6346B4DDA13C97C4C0BE190A1D1F519E.encrypted.properties
 gradle clean build -i
-gradle deploy -d
+gradle deploy -d -PcredentialsPassphrase=$CREDENTIALS_ENV
