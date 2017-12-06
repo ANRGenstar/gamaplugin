42888db29d325e77314b1691f77222999e54e8ac
rey-coyrehourcq <reyman@localhost.localdomain>
2017-12-06 13:04:01 +0100

    fix travis

--- travisbuild.sh
+++ travisbuild.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-mkdir .gradle/
-cd .gradle
-touch gradle.6346B4DDA13C97C4C0BE190A1D1F519E.encrypted.properties > genstarpassw=xMY2d63UAUixj3m/AXuCTg\=\=
-cd
-gradle clean build
+echo "genstarpassw=xMY2d63UAUixj3m/AXuCTg\=\=" >> ~/.gradle/gradle.6346B4DDA13C97C4C0BE190A1D1F519E.encrypted.properties
